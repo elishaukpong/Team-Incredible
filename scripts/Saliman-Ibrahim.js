61bb8e2b68233dc2c8606b4f9456d27a05b8295d
Zeeson <olabrazanislam@gmail.com>
2020-06-05 10:14:12 -0700

    My script Slack ID: Zeeson

---
+++ scripts/Saliman-Ibrahim.js
@@ -0,0 +1,8 @@
+let name = "Saliman Ibrahim";
+let HNGid = "HNG-02450";
+let Langauage = "JavaScript";
+let Email = "olabrazanislam@gmail.com";
+
+
+
+console.log("Hello World, this is " + name + " with HNGi7 ID " + HNGid + " and email " + Email + " using " + Langauage + " for stage 2 task.");
