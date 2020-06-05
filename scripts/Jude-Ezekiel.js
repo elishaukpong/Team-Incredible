6b798f685ecf8adb48573836c1d468b34aed9a1c
jude-Ezekiel <billyjude007@gmail.com>
2020-06-05 15:22:29 +0100

    feature: script file

---
+++ scripts/Jude-Ezekiel.js
@@ -0,0 +1,7 @@
+function helloWorld() {
+  console.log(
+    "Hello World, this is Jude Ezekiel with HNGi7 ID HNG-00817 and email ezekiel.jude5@gmail.com using JavaScript for stage 2 task."
+  );
+}
+
+helloWorld();
