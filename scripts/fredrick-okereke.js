c0abf90176086a521b480ec6a8f27d21714fc7dc
Fredrick Kelly <38326256+iamfredrickokereke@users.noreply.github.com>
2020-06-05 15:45:54 +0100

    Create fredrick-okereke.js

---
+++ scripts/fredrick-okereke.js
@@ -0,0 +1,11 @@
+let details = {
+    fullName: "Fredrick kelly",  
+    language: "JavaScript",
+    hngID: "HNG-00468",
+    email: "kellyonline2016@gmail.com"
+};
+  
+const message = `Hello World, this is ${details.fullName} with HNGi7 ID ${details.hngID} and email ${details.email} using ${details.language} for stage 2 task.`;
+
+console.log(message);
+  
