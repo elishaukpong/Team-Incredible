07ccc2368d2333318c2471a791ff739472637b0a
Callistus Enekhaze <44670996+OsiKing@users.noreply.github.com>
2020-06-05 16:35:55 +0100

    Added .

--- scripts/Callistus-Enekhaze.php
+++ scripts/Callistus-Enekhaze.php
@@ -6,7 +6,7 @@ $language = "PHP";
 
 function task($fullname, $email, $ID, $language){
 
-    $msg =  "Hello World, this is $fullname with HNGi7 ID $ID and email $email using $language for stage 2 task";
+    $msg =  "Hello World, this is $fullname with HNGi7 ID $ID and email $email using $language for stage 2 task.";
     return $msg;
 }
 echo task($fullname, $email, $ID, $language);
