48cfce7c0fd940ba1ea34d60a6712b6b97dce1e2
unknown <mercyjemosop@gmail.com>
2020-06-05 21:40:44 +0300

    Task for stage 2 team incredible

--- scripts/kipyegon-mercy.php
+++ scripts/kipyegon-mercy.php
@@ -4,6 +4,6 @@ $name="Kipyegon Mercy";
 $email="mercyjemosop@gmail.com";
 $language="PHP";
 $hello=("Hello World, this is ".$name." with HNGi7 ID ".$id." and email ".$email." using ".$language." for stage 2 task.");
-print $hello);
+print ($hello);
 
 ?>
