cd3b74b135a20b64ca1fe5d7fba4f5a361d14ba6
Tobi Osimosu <Osimosutobi@gmail.com>
2020-06-05 16:32:16 +0100

    Update nancyben.js

--- scripts/nancyben.js
+++ scripts/nancyben.js
@@ -4,4 +4,4 @@ let hngID = "HNG-06537";
 let language = "javaScript";
 let email = "medulab10@gmail.com";
 
-console.log(`Hello World, this is ${firstName} ${lastName}  with HNGi7 ID ${hngID} and email ${email} using ${language} for stage 2 task.`);
+console.log(`Hello World, this is ${firstName} ${lastName} with HNGi7 ID ${hngID} and email ${email} using ${language} for stage 2 task.`);
