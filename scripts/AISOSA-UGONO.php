174b8e9da28b980638f31ac4476fd7193e6d9deb
akpe2003 <aes_akpe@yahoo.ca>
2020-06-05 15:50:26 +0100

    Create AISOSA-UGONO.php

---
+++ scripts/AISOSA-UGONO.php
@@ -0,0 +1,7 @@
+<?php
+$full_name="Aisosa Ugono";
+$id="HNG-00918";
+$language="PHP";
+$email="akpeugono@gmail.com";
+echo "Hello World, this is $full_name with HNGi7 ID $id and email $email using $language for stage 2 task.";
+?>
