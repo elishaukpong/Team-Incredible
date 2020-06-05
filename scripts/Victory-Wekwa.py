8daec0fa2381c2178463441cb860849e8da633a7
VictoryWekwa <victorywekwa@gmail.com>
2020-06-05 18:10:17 +0100

    script/Victory-Wekwa

---
+++ scripts/Victory-Wekwa.py
@@ -0,0 +1,6 @@
+firstName="Victory"
+LastName="Wekwa"
+id="HNG-01926"
+Language="Python"
+email="victorywekwa@gmail.com"
+print("Hello World, this is"+" "+firstName +" "+LastName+" "+"with HNGi7 ID" +" "+ id + " " +"and email" + " " + email+" " + "using" +" " +Language + " " + "for stage 2 task.")
