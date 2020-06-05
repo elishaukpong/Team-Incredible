2c4f1f9610b9316bfd9e6bbdac2e7e344023d126
Nusrah Farri-Ghazal <nusrahfarri@gmail.com>
2020-06-05 17:20:49 +0100

    Add files via upload

---
+++ scripts/nusrah-farri-ghazal.js
@@ -0,0 +1,3 @@
+var myLanguage = "JavaScript";
+
+console.log("Hello World, this is Nusrah Farri-Ghazal with HNGi7 ID HNG-00310 and email nusrahfarri@gmail.com using " + myLanguage + " for stage 2 task.")
