de3965e6e630235ac5aef12e2e08e869d1cb4ee2
Adeleke Isaiah <iadeleke01@gmail.com>
2020-06-05 15:30:28 +0100

    Added full stop

--- scripts/david-ajawu.py
+++ scripts/david-ajawu.py
@@ -1,5 +1,5 @@
 def cool_intro_function():
-    print('Hello World, this is {0} with HNGi7 ID {1} and email {3} using {2} for stage 2 task'\
+    print('Hello World, this is {0} with HNGi7 ID {1} and email {3} using {2} for stage 2 task.'\
     	  .format('David Ajawu', 'HNG-04265', 'python', 'ajawudavid@gmail.com'))
 
 
