819e449060f3227c4c4c070244e26b5ce58978ef
Vivyanne-504 <66225228+Vivyanne-504@users.noreply.github.com>
2020-06-05 19:57:49 +0100

    Update vivian-nwosu.py

--- scripts/vivian-nwosu.py
+++ scripts/vivian-nwosu.py
@@ -1,5 +1,6 @@
-name = "Vivian Nwosu"
-    ID = "HNG-00494"
-    email = "vivianwosu05@gmail.com "
-    language="Python"
-    print('Hello World, this is' +name+'with HNGi7 ID'+ID+ 'and email' +email+ 'using'+language+ 'for stage 2 task')
+full_name='Vivian Nwosu'
+HNG_ID='HNG-00494'
+email= 'viviannwosu05@gmail.com'
+language= 'python'
+print('Hello World, this is ' + full_name + ' with HNGi7 ID ' + HNG_ID
+      + ' and email ' + email + ' using ' + language + ' for stage 2 task.')
