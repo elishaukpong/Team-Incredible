a35b105ef89688316e2f7592d1fce41523202f37
joyodeyemi <61966766+joyodeyemi@users.noreply.github.com>
2020-06-05 17:13:48 +0100

    Joy changed somethings

--- scripts/Joy-Odeyemi.js
+++ scripts/Joy-Odeyemi.js
@@ -1,10 +1,6 @@
-var greeting = "Hello World, " ;
+
 var name = " Joy Odeyemi ";
 var id = " HNG-02877 ";
 var email = " joybella4eva@gmail.com ";
 var language = " JavaScript ";
-var task = " stage 2 task.";
-
-var final = greeting + "this is" + name + "with HNGi7 ID" + id + "and email" + email + "using" + language + "for" + task;
-
-console.log(final)
+console.log('Hello World, this is' + name + 'with HNGi7 ID' + id + 'and email' + email + 'using' + language + 'for stage 2 task.')
