b84fa473576d1457221cbb701d0a5f73b7366e23
Chisom Nwokwu <ceekay2a7@gmail.com>
2020-06-05 16:57:29 +0100

    Updated File

---
+++ scripts/chisom-nwokwu.js
@@ -0,0 +1,6 @@
+const name = "Chisom Nwokwu";
+const id = "HNG-01667";
+const language = "javascript";
+const email = "chisomnwokwu09@gmail.com";
+
+console.log(`Hello World, this is ${name} with HNGi7 ID ${id} and email ${email} using ${language} for stage 2 task.`);
