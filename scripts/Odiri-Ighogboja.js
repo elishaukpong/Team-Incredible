70b8eb19832cd1fa9915895b38849b08e95c7b86
odizee <66241926+odizee@users.noreply.github.com>
2020-06-05 15:51:24 +0100

    Odiri_Ighogboja_FIle

---
+++ scripts/Odiri-Ighogboja.js
@@ -0,0 +1,8 @@
+var odizeeDetails = {
+    "name" : "Ighogboja Odiri",
+    "id" : "HNG-01021",
+    "language" : "JavaScript",
+    "email" : "odizeeplatform@gmail.com",
+
+}
+    console.log("Hello World, this is " + odizeeDetails.name + " with HNGi7 ID " + odizeeDetails.id + " and email " + odizeeDetails.email + " using " + odizeeDetails.language + " for stage 2 task.");
