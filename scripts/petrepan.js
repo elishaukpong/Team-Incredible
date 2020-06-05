d94ff81c8c2ec1fad606ee54b8c4c060d3c53674
petrepan <peterawotola@gmail.com>
2020-06-05 15:31:30 +0100

    profile added

---
+++ scripts/petrepan.js
@@ -0,0 +1,10 @@
+const name = "Peter Awotola";
+const language = "JavaScript";
+const hngID = "HNG-04950";
+const email = "peterawotola@gmail.com";
+
+const myProfile = () => {
+  console.log(`Hello World, this is ${name} with HNGi7 ${hngID} using ${language} for stage 2 task email ${email}.`)
+};
+
+myProfile();
