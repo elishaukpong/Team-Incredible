da237cea5f5288f72c59ae57630946baa6685c66
spencer29 <66229033+spencer29@users.noreply.github.com>
2020-06-05 16:20:58 +0100

    Updated Spencer.py
    This has been updated and running perfectly on my machine. Please do give me a precise issue with it if any.

--- scripts/Spencer.py
+++ scripts/Spencer.py
@@ -1,1 +1,1 @@
-print ("Hello World, This is Chukwuemeka Samuel Egwuonwu with HNGi7 ID HNG-03107 and email chukwuemeka.samuel29@gmail.com using Python for stage 2 task")
+print("Hello world, This is Chukwuemeka Samuel With HNGi7 ID HNG-03107 and Email chukwuemeka.samuel29@gmail.com using Python for stage two task")
