11e9779fff8fce8c973a73ac368972b0b8315c56
bauxitex3 <66308671+bauxitex3@users.noreply.github.com>
2020-06-05 10:17:01 -0400

    BigBello

---
+++ scripts/bigbello.py
@@ -0,0 +1,4 @@
+def main():
+    print("Hello World, this is Malcom Bello with HNGi7 ID HNG-02832 and email bauite.x3@gmail.com using javascript for stage 2 task")
+if __name__ == main():
+    main()
