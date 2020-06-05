103b53918333bf703993b6487ce4de7059f62ea6
Jonah Ifeoma <ifyajoke@gmail.com>
2020-06-05 17:09:24 +0100

    Feat: added Ifeoma Jonah script file

---
+++ scripts/Ifeoma-Jonah.js
@@ -0,0 +1,9 @@
+function introduceIntern(){
+    let fullName = "Ifeoma Jonah";
+    let hngId = "HNG-04143";
+    let email = "ifyajoke@gmail.com";
+    let language = "Javascricpt";
+    console.log(`Hello World, this is ${fullName} with HNGi7 ID ${hngId} and email ${email} using ${language} for stage 2 task.`)
+}
+
+introduceIntern();
