421da603c42410dedf81025d1b2821f181455596
bauxitex3 <66308671+bauxitex3@users.noreply.github.com>
2020-06-05 07:19:59 -0700

    Delete bigbello.py

--- scripts/bigbello.py
+++
@@ -1,4 +0,0 @@
-def main():
-    print("Hello World, this is Malcom Bello with HNGi7 ID HNG-02832 and email bauite.x3@gmail.com using javascript for stage 2 task")
-if __name__ == main():
-    main()
