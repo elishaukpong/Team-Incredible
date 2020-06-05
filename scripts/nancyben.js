5a8b3ac4d1b15fb04e9aa98daf716f8b176a0ec9
nancyben <66383239+nancyben@users.noreply.github.com>
2020-06-05 16:09:52 +0100

    Create nancyben.js

---
+++ scripts/nancyben.js
@@ -0,0 +1,7 @@
+let firstName = "Nancy";
+let lastName = "Ben";
+let hngID = "HNG-06537";
+let language = "javaScript";
+let email = "medulab10@gmail.com";
+
+console.log(`Hello World, this is ${firstName} ${lastName}  with HNGi7 ID ${hngID} and email ${email} using ${language} for stage 2 task.`);
