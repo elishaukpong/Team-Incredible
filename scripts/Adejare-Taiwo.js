fc0f936e24c6d987b5173bd4e117d937021b7b6f
jboy457 <55572807+jboy457@users.noreply.github.com>
2020-06-05 15:35:04 +0100

    Jboy hng team-incredible task

---
+++ scripts/Adejare-Taiwo.js
@@ -0,0 +1,6 @@
+var name = "Taiwo Adejare Emmanuel";
+var hng_id = "HNG-04684";
+var lang = "JavaScript";
+var email = "adejareemma@gmail.com"
+
+console.log("Hello World, this is " + name + " and email " + email + " with HNGi7 ID " + hng_id + " using " + lang + " for stage 2 task.");
