41bf2e490351099dfcede04a186c7181d22c2095
thetabyte <seunlatunde@gmail.com>
2020-06-05 17:56:57 +0100

    This is my script, Slack ID: thetabyte

---
+++ scripts/Seun-Latunde.js
@@ -0,0 +1,8 @@
+const hngiRegName="Seun Latunde";
+const userEmail="seunlatunde@gmail.com";
+const hngId="HNG-01312";
+const langStack="Javascript";
+console.log(`Hello World, this is ${hngiRegName} with HNGi7 ID ${hngId} and email ${userEmail} using ${langStack} for stage 2 task.`);
+
+//Output String Format below;
+//"Hello World, this is Elisha Ukpong with HNGi7 ID HNG-03659 and email ishukpong418@gmail.com using JavaScript for stage 2 task."
