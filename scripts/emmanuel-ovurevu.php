9d895976c0700524ebad750d3d5ba456bf3485d5
Emmanuel Yakubu <emmanuel.ovurevu@gmail.com>
2020-06-05 16:47:25 +0100

    Emmanuel Ovurevu created his script file

---
+++ scripts/emmanuel-ovurevu.php
@@ -0,0 +1,9 @@
+<?php
+$name = 'Emmanuel Ovurevu';
+$hngId = 'HNG-06451';
+$email = 'emmanuel.ovurevu@gmail.com';
+$scriptLanguage = 'php';
+
+$output = "Hello World, this is {$name} with HNGi7 ID {$hngId} and email {$email} using {$scriptLanguage} for stage 2 task.";
+
+echo $output;
