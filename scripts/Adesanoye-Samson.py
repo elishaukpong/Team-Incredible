7a0cbbe509b170223b34192e5d7b15dc4990c65f
Adesanoye Samson <adesanoye.samson@yahoo.com>
2020-06-05 19:29:09 +0100

    python updated script from Adesanoye Samson

--- scripts/Adesanoye-Samson.py
+++ scripts/Adesanoye-Samson.py
@@ -9,6 +9,6 @@ data = {
     "email": "adesanoye.samson1@gmail.com"
 }
 
-string = "Hello World, this is " + data['name'] + " with HNGi7 ID " + data['hng_id'] + " and email " + data['email'] using Python for stage 2 task. 
+string = "Hello World, this is " + data['name'] + " with HNGi7 ID " + data['hng_id'] + " and email " + data['email'] + " using Python for stage 2 task." 
 
 print(string)
