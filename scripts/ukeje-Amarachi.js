8aeb9b0d52eb19d043bf97595e4ce68659e7dee7
Amarabest <ukejeamarajovita@gmail.com>
2020-06-05 19:40:57 +0100

    My task 2 edited team format

--- scripts/ukeje-Amarachi.js
+++ scripts/ukeje-Amarachi.js
@@ -3,7 +3,7 @@ var printMessage = function() {
     var hngid = 'HNG-02625';
     var email = 'ukejeamarajovita@gmail.com';
     var lang = 'JavaScript';
-    var message = 'Hello World, this is '+ name + ' with HNGi7 ID ' + hngid + ' using ' + lang + ' for stage 2 task and ' +email;
+    var message = 'Hello World, this is '+ name + ' with HNGi7 ID ' + hngid + " and email " + email + ' using ' + lang + ' for stage 2 task.';
     console.log(message);
-}
-printMessage();
+ }
+ printMessage();
