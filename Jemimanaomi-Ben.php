ae9a34b792ba905c101787e7b57d6cadede604c2
JemimaNaomiBen <54313529+JemimaNaomiBen@users.noreply.github.com>
2020-06-05 15:33:00 +0100

    Add files via upload
    Incredible task2 commit

---
+++ Jemimanaomi-Ben.php
@@ -0,0 +1,8 @@
+<?php
+
+$mesg1 = "Hello World, this is JemimaNaomi Godwin Ben with HNGi7 ID";
+$mesg2 = "HNG-02526 and email benjemimanaomi@gmail.com using php for stage 2 task.";
+
+echo $mesg1 . " " . $mesg2;
+
+?>
