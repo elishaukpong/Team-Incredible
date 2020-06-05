76d2d4366791c599a1f833f46f4efb1f5c91459c
Elisha Ukpong <Ishukpong418@gmail.com>
2020-06-05 22:00:33 +0100

    fixed

--- scripts/Mary-Enoch.js
+++ scripts/Mary-Enoch.js
@@ -1,4 +1,4 @@
-function intern_info() {
+function intern_info(){
    return "Hello World, this is Enoch Mary with HNGi7 ID HNG-00484 and email morevaoghene@gmail.com using JavaScript for stage 2 task."
    }
-   console.log(intern_info());
+   console.log(intern_info())
--- scripts/mary-enoch.js
+++
@@ -1,4 +0,0 @@
-function intern_info(){
-   return "Hello World, this is Enoch Mary with HNGi7 ID HNG-00484 and email morevaoghene@gmail.com using JavaScript for stage 2 task."
-   }
-   console.log(intern_info())
