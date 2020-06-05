3337593721a3ae31b9360b7ed82762d30c221576
SteveOye <seyioyebanjistephen@gmail.com>
2020-06-05 15:50:40 +0100

    switch to dart

--- scripts/SteveOS.dart
+++ scripts/SteveOS.dart
@@ -2,6 +2,7 @@ var fullName = 'Stephen Oyebanji';
 var ID = 'HNG-00014';
 var email = 'seyioyebanjistephen@gmail.com';
 var language= 'Dart';
+
 main() {
   
   var output = 'Hello World, this is $fullName with HNGi7 ID $ID and email $email  using $lang for stage 2 task';
