fa038aa6d7d39ed4960c7b9aa303e942de89e89c
JemimaNaomiBen <54313529+JemimaNaomiBen@users.noreply.github.com>
2020-06-05 15:43:28 +0100

    Delete Jemimanaomi-Ben.php

--- scripts/Jemimanaomi-Ben.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$mima1 = "Hello World, this is JemimaNaomi Godwin Ben with HNGi7 ID";
-$mima2 = "HNG-02526 and email benjemimanaomi@gmail.com using php for stage 2 task.";
-
-echo $mima1 . " " . $mima2;
-
-?>
