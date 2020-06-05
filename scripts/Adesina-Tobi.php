43ad2a2ada70b6350149bad3fc50c6c2b79dbc17
samueluchendu <samueluchendu47@gmail.com>
2020-06-05 23:52:22 +0100

    Add my script

---
+++ scripts/Adesina-Tobi.php
@@ -0,0 +1,20 @@
+<?php
+
+
+function myHng(){
+
+     $email = 'oluwatobi275@gmail.com';
+     $id = 'HNG-04469';
+     $language = 'PHP';
+     $fullname = 'Adesina Tobi';
+
+
+  echo 'Hello World, this is '.$fullname. ' with HNGi7 ID '.$id.' and email '.$email.' using '.$language.' for stage 2 task';
+
+}
+
+myHng();
+
+
+
+ ?>
