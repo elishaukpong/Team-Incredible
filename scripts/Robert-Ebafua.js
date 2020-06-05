be8d6f6ae9c0e0568cb467ba783594fe1861a2ee
kelrob <test>
2020-06-05 15:28:58 +0100

    Eromosele

---
+++ scripts/Robert-Ebafua.js
@@ -0,0 +1,11 @@
+const myFunction = () => {
+
+    let fullname = 'Robert Ebafua';
+    let hngId = 'HNG-00335';
+    let devLang = 'Javascript';
+    let email  = 'robertebafua@gmail.com';
+
+    console.log(`Hello World, this is ${fullname} with HNGi7 ID ${hngId} and email is${email} using ${devLang} for stage 2 task.`);
+};
+
+myFunction();
