0672cff761b6561a334d58970a952c705652783e
Sommie09 <ceekay2a7@gmail.com>
2020-06-05 17:10:03 +0100

    Chisom Nwokwu Task

---
+++ scripts/chisom-nwokwu.js
@@ -0,0 +1,11 @@
+
+const details = () => {
+    
+    const name = 'Chisom Nwokwu';
+    const HNG_ID = 'HNG-01667';
+    const email = 'chisomnwokwu09@gmail.com';
+    const language  = 'JavaScript';
+    return `Hello World, this is ${ name } with HNGi7 ID ${ HNG_ID } and email ${ email } using ${ language } for stage 2 task`
+}
+
+console.log(details());
