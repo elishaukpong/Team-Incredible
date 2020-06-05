03922b14c048a188567c21ee2690ae627eafedc2
Pranay Yenagandula <Pranay41@github.com>
2020-06-05 20:24:05 +0530

    Added Pranay-Yenagandula.py

---
+++ scripts/Pranay-Yenagandula.py
@@ -0,0 +1,10 @@
+#Pranay-Yenagandula 
+
+class display_text:
+    def __init__(self):
+        self.output="Hello World, this is Pranay Yenagandula with HNGi7 ID HNG-01542 and email pranay41@gmail.com using Python for stage 2 task."
+    def show(self):
+        print(self.output)
+
+d=display_text()
+d.show()
