a14cd1ee9522a1cc536d3013a25e054439eeed37
JCRichLee <54782441+JCRichLee@users.noreply.github.com>
2020-06-05 22:25:28 +0100

    Create Richard - Adeyele.js

---
+++ scripts/Richard - Adeyele.js
@@ -0,0 +1,5 @@
+var mail = 'radeyele@gmail.com';
+var dev_name = 'Richard Adeyele';
+var hngid = 'HNG-01484';
+var language = 'Javascript';
+console.log("Hello World, this is " + dev_name + " with HNGi7 ID " + hngid + " and email " + mail + " using " + language + " for stage 2 task.");
