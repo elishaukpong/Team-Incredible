4f2ac6aa09dfab00c214514e7a8a80342bcd42d2
Vivyanne-504 <66225228+Vivyanne-504@users.noreply.github.com>
2020-06-05 16:03:54 +0100

    Create vivian-nwosu.py

---
+++ scripts/vivian-nwosu.py
@@ -0,0 +1,6 @@
+full_name='Vivian Nwosu'
+HNG_ID='HNG-00494'
+email= 'viviannwosu05@gmail.com'
+language= 'python '
+print('Hello World, this is ' + full_name + ' with HNGi7 ID ' + HNG_ID
+      + ' and email ' + email + ' using ' + language + ' for stage 2 task.')
