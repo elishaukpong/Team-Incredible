4cb4046d31f921860cb6d27698956f76ffbfd6f2
Adedeji <sademolaadedeji@gmail.com>
2020-06-05 19:45:17 +0100

    Updated the script, removing f string and using string format'

--- scripts/Saheed-Adedeji.py
+++ scripts/Saheed-Adedeji.py
@@ -1,11 +1,11 @@
-name = 'Adedeji Saheed'
+name = 'Saheed Adedeji'
 hng_id = 'HNG-02648'
 email = 'sademolaadedeji@gmail.com'
 language = 'Python'
 
 
 def intro(name, hng_id, language, email):
-    return f'Hello World, this is {name} with HNGi7 ID {hng_id} and email {email} using {language} for stage 2 task.'
+    return "Hello World, this is {} with HNGi7 ID {} and email {} using {} for stage 2 task.".format(name, hng_id, email, language)
 
 
 print(intro(name, hng_id, language, email))
