87f359e99e90dc432310a2a9e634c1229050ad5f
drizzt401 <faithudoh71@gmail.com>
2020-06-05 21:50:54 +0100

    Update and rename faithudoh.js to Faith-Udoh.js

---
+++ scripts/Faith-Udoh.js
@@ -0,0 +1,9 @@
+function helloWorld(){ 
+var email = "faithudoh71@gmail.com"; 
+var fullName = "Udoh Faith"; 
+var hngID = "HNG-05995"; 
+var prefLang = "Javascript"
+var output = `Hello World, this is ${fullName} with HNGi7 ID ${hngID} and email ${email} using ${prefLang} for stage 2 task.` 
+return (output) 
+} 
+console.log(helloWorld());
--- scripts/faithudoh.js
+++
@@ -1,8 +0,0 @@
-function helloWorld(){ 
-var email = "faithudoh71@gmail.com"; 
-var fullName = "Udoh Faith"; 
-var hngID = "HNG-05995"; 
-var output = `Hello World, this is ${fullName} with HNGi7 ID ${hngID} and email ${email} using Javascript for stage 2 task` 
-return (output) 
-} 
-console.log(helloWorld());
