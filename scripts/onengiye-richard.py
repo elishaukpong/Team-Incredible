204a82b2a94385edeed41561b342d5e579f26b5d
Onengiye Richard <klevamane@gmail.com>
2020-06-05 21:05:52 +0100

    update

--- scripts/onengiye-richard.py
+++ scripts/onengiye-richard.py
@@ -1,1 +1,13 @@
-print("Hello World, this is Onengiye Richard with HNGi7 ID HNG-02932 and email onengiye.richard@gmail.com using Python for stage 2 task.")
+name = 'Onengiye Richard'
+hng_id = 'HNG-02648'
+email = 'onengiye.richard@gmail.com'
+language = 'Python'
+
+
+def intro(name, hng_id, language, email):
+    return f'Hello World, this is {name} with HNGi7 ID {hng_id} and email {email} using {language} for stage 2 task.'
+
+
+print(intro(name, hng_id, language, email))
+
+
