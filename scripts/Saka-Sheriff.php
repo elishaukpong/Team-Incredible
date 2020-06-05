7a224ba63947822cafe864ececd9d871a1e87683
Saka Sheriff Alade <sheriffdeenalade@gmail.com>
2020-06-05 17:56:38 +0100

    Update Saka-Sheriff.php
    This is my script, Slack ID: U014XL8L89F

--- scripts/Saka-Sheriff.php
+++ scripts/Saka-Sheriff.php
@@ -1,3 +1,3 @@
 <?php
-	echo "Hello World, this is Saka Sheriff Alade with HNGi7 ID NG-02701 and email adetunji_alade2005@yahoo.com using PHP for stage 2 task.";
+	echo "Hello World, this is Saka Sheriff Alade with HNGi7 ID HNG-02701 and email adetunji_alade2005@yahoo.com using PHP for stage 2 task.";
 ?>
