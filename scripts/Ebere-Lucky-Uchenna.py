cc52eedb25dc685decbdd1f81fb03273ef16ec16
Crazelu <eberelucky64@gmail.com>
2020-06-05 19:29:23 +0100

    reverted script to be compatible with python 2

--- scripts/Ebere-Lucky-Uchenna.py
+++ scripts/Ebere-Lucky-Uchenna.py
@@ -1,4 +1,4 @@
 def introduce(name, hngid, email):
-    return f"Hello World, this is {name} with HNGi7 ID {hngid} and email {email} using Python for stage 2 task."
+    return "Hello World, this is {} with HNGi7 ID {} and email {} using Python for stage 2 task.".format(name, hngid, email)
 
 print(introduce('Ebere Lucky Uchenna', "HNG-01461", "eberelucky64@gmail.com"))
