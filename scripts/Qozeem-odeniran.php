eb83525e3122179b629b099147d5427056c9c91c
Qozeem Odeniran <muhdqozeem@gmail.com>
2020-06-05 17:00:24 +0100

    New update

---
+++ scripts/Qozeem-odeniran.php
@@ -0,0 +1,8 @@
+<?php
+$fullname = "Qozeem Odeniran";
+$id = "HNGi7-01681";
+$language = "PHP";
+$email = "muhdqozeem@gmail.com";
+
+echo "Hello World, this is $fullname with HNGi7 ID $id and email $email using $language for stage 2 task.";
+?>
