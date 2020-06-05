b491be75aba9540d68d4fcddd19432b4bd7d3e9b
Nisha Mehra <64705808+nishamehra@users.noreply.github.com>
2020-06-05 07:31:48 -0700

    Add files via upload

---
+++ scripts/Nisha-Mehra.py
@@ -0,0 +1,4 @@
+name = "Nisha Mehra"
+hngid = "HNG-01532"
+email_address = "nishamehra2052@gmail.com"
+print("Hello World, this is " + name + " with HNGi7 ID " + hngid +" and email " + email_address + " using Python for stage 2 task. ")
