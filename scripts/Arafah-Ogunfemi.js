bbf6d1aeade4db1ed335e3b54260c4f13dbbfc6c
Elisha Ukpong <Ishukpong418@gmail.com>
2020-06-05 21:20:35 +0100

    fixing

--- scripts/Arafah-Ogunfemi.js
+++ scripts/Arafah-Ogunfemi.js
@@ -1,12 +1,12 @@
 const userDetails = {
   firstName: "Arafah",
   lastName: "Ogunfemi",
-  id: "HNG-05018",
+  hid: "HNG-05018",
   email: "ayobamiarafat09@gmail.com",
   language: "Javascript",
 };
 
-const output = ({ firstName, lastName, id, language, email }) =>
-  `Hello World, this is ${firstName} ${lastName} with HNGi7 ID ${id} and email ${email} using ${language} for stage 2 task.`;
+const output = ({ firstName, lastName, hid, language, email }) =>
+  `Hello World, this is ${firstName} ${lastName} with HNGi7 ID ${hid} and email ${email} using ${language} for stage 2 task.`;
 
 console.log(output(userDetails));
--- scripts/george-owoicho.py
+++ scripts/george-owoicho.py
@@ -8,6 +8,6 @@ email="owoichogeorge4400@gmail.com"
 
 # create a python dictionary to hold the user details
 def interndata():
-    userdetails = json.dumps("Hello World, this is "+name+" with HNGI7 ID: "+id+" and email "+email+" using Python for stage 2 task.")
+    userdetails = json.dumps("Hello World, this is "+name+" with HNGI7 ID "+id+" and email "+email+" using Python for stage 2 task.")
     print (userdetails)
 interndata()
