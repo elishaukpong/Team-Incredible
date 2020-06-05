9c2c73735c5e109b677685284e2d73403bde3087
Anuoluwapo <oderinloanuoluwapo@gmail.com>
2020-06-05 19:20:30 +0100

    I Anuoluwapo added a php script

---
+++ scripts/Anuoluwapo-Oderinlo.php
@@ -0,0 +1,7 @@
+<?php 
+  $name = "Anuoluwapo Oderinlo";
+  $email = "oderinloanuoluwapo@gmail.com";
+  $hngID = "HNG-01297";
+
+  print "Hello World, this is ".$name." with HNGi7 ID ".$hngID." and email ".$email." using Php for stage 2 task";
+?>
