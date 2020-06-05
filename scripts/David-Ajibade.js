5686d13807f4f55327e7445ad4d6244b7d877572
inioluwadavid <63739486+inioluwadavid@users.noreply.github.com>
2020-06-05 15:33:03 +0100

    Task  2 task completed
    Task completed using JavaScript

---
+++ scripts/David-Ajibade.js
@@ -0,0 +1,11 @@
+/* variables declaration
+to make the returned string dynamic, it's useful to declare variables for parts of the string that can change, e.g. Name, HNG Id, language and stage number 
+*/
+
+var internName = "David Ajibade";
+var internID = "HNG-03857";
+var taskStage = 2;
+var internLanguage = "JavaScript";
+var internEmail = "ajidaveini@gmail.com";
+
+console.log(`Hello World, this is ${internName} with HNGi7 ID ${internID} and email ${internEmail} using ${internLanguage} for stage ${taskStage} task`);
