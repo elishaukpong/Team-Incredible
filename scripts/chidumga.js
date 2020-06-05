1d20b72d859f14e0555799ba6a9e977f32dd078e
kelvonwuka <66382278+kelvonwuka@users.noreply.github.com>
2020-06-05 17:48:23 +0100

    Create chidumga.js

---
+++ scripts/chidumga.js
@@ -0,0 +1,7 @@
+const firstName = "Onwuka"
+const lastName = "Chidumga"
+const hngID = "HNG-06529"
+const language = "javascript"
+const email = "kelvinonwuka8@gmail.com"
+
+console.log(`Hello World, this is ${firstName} ${lastName} with HNGi7 ID ${hngID} and email ${email} using ${language} for stage 2 task.`);
