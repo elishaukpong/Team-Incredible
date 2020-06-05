b9af43c13326f20cfdda66261b44e90adb2f141e
David Adigwu <davidadigwu@gmail.com>
2020-06-05 15:36:11 +0100

    Added my script file

---
+++ scripts/David-Adigwu.php
@@ -0,0 +1,12 @@
+<?php
+
+//variables
+
+$fullName = "Adigwu David Uche";
+$ID = "HNG-00567";
+$email = "davidadigwu@gmail.com";
+$scriptLanguage = "PHP";
+
+echo "Hello World, this is $fullName with HNGi7 ID $ID and email $email using $scriptLanguage for stage 2 task";
+
+?>
