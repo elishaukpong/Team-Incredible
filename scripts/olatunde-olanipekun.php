e03ef49ae41f997b97f355e0eb2b2eb46799c1c6
orlawealth <luv4junior@yahoo.com>
2020-06-05 20:37:21 +0100

    add my script

---
+++ scripts/olatunde-olanipekun.php
@@ -0,0 +1,7 @@
+<?php 
+	$detail = array("Olanipekun Olatunde", "HNG-03185", "joshuaolanipekun1@gmail.com", "PHP");
+	
+	echo  "Hello World, this is ".$detail[0]." with HNGi7 ID " .$detail[1]. " and email " .$detail[2]." using ".$detail[3]." for stage 2 task";
+
+
+ ?>
