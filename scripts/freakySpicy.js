6167265b446b2b54459d0ca3ab8a467ef9ba21ae
VictoriaAde <adedayovicky123@gmail.com>
2020-06-05 17:03:36 +0100

    Freaky

--- scripts/freakySpicy.js
+++ scripts/freakySpicy.js
@@ -1,4 +1,4 @@
-var data = {"first_name":"Victoria", "last_name":"Adedayo", "id":"HNG-01723", "email":"adedayovicky@gmail.com", "language":"Javascript"};
+var data = {"first_name":"Victoria", "last_name":"Adedayo", "id":"HNG-01723", "email":"adedayovicky123@gmail.com", "language":"Javascript"};
 
 var j = (data);
 console.log("Hello World, this is " + j.first_name + " " + j.last_name + " with HNGi7 ID " + j.id + " and email " + j.email + " using " + j.language + " for stage 2 task.");
