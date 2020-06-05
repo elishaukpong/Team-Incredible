e6c7e279521b78507d4ccedcea578a464a4c97b6
sahmie <sahmienwoko@gmail.com>
2020-06-05 15:36:59 +0100

    added my script

---
+++ scripts/Samuel-Nwoko.js
@@ -0,0 +1,10 @@
+const fullName = "Samuel Nwoko";
+const id = "HNG-01730";
+const emailAddr = "sahmienwoko@gmail.com";
+const language = "JavaScript";
+
+personalIfo = ()=>{
+  console.log(`Hello World, this is ${fullName} with HNGi7 ID ${id} and email ${emailAddr} using ${language} for stage 2 task.`)
+};
+
+personalIfo();
