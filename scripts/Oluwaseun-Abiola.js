7133add8a6dd9cf0c7de4cc06ce34495a9c5fa17
Oluwaseun <oluabiolaseun@gmail.com>
2020-06-05 18:12:35 +0100

    Add Oluwaseun Abiola's script

---
+++ scripts/Oluwaseun-Abiola.js
@@ -0,0 +1,9 @@
+const hello = () => {
+  const fullName = "Oluwaseun Abiola";
+  const hngId = "HNG-04768";
+  const language = "JavaScript";
+  const email = "olabiola@ecobank.com";
+  console.log(`Hello World, this is ${fullName} with HNGi7 ID ${hngId} and email ${email} using ${language} for stage 2 task.`);
+};
+
+hello();
