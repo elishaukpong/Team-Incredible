bd230a98967069a99652b249721dba45b947584d
Oluwafemijohn <femiogundipe01@gmail.com>
2020-06-05 17:41:16 +0100

    team_incredible modified script

---
+++ scripts/oluwafemi_j.js
@@ -0,0 +1,9 @@
+var email = "femiogundipe01@gmail.com";
+var name = "Oluwafemi Ogundipe";
+var hng_id  = "HNG-02800";
+var language = "Javascript";
+let myfunction = function(){
+    console.log("Hello World, this is " + name + " with HNGi7 ID " + hng_id + " and email "+ email + " using " + language +" for stage 2 task.")
+}
+myfunction()
+
