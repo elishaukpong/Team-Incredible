e6b069c8f61743382fac6245329b5bbf381a81bb
Michael Whitener <mjawhitener@gmail.com>
2020-06-05 14:44:48 -0500

    Script

---
+++ scripts/Michael-Whitener.php
@@ -0,0 +1,9 @@
+<?php
+
+$fullName = "Michael Whitener";
+$emailAddress = "mjawhitener@gmail.com";
+$hngID = "HNG-01387";
+$language = "PHP";
+$htmlOutput = "Hello World, this is $fullName with HNGi7 ID $hngID and email $emailAddress using $language for stage 2 task";
+
+echo $htmlOutput;
