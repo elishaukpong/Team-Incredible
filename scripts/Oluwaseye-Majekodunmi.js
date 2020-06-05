0508aff5735ac790c689d6097981942decc7d53c
Iamsheye <65172089+Iamsheye@users.noreply.github.com>
2020-06-05 15:59:10 +0100

    Update Oluwaseye-Majekodunmi.js

--- scripts/Oluwaseye-Majekodunmi.js
+++ scripts/Oluwaseye-Majekodunmi.js
@@ -4,7 +4,7 @@ const test = () => {
   const hngId = 'HNG-00095';
   const lang = 'JavaScript';
   const mail = 'seyepelumi@gmail.com';
-  return `Hello World, this is ${name} with HNGi7 ID ${hngId} and email ${mail} using ${lang} for stage 2 task`
+  return `Hello World, this is ${name} with HNGi7 ID ${hngId} and email ${mail} using ${lang} for stage 2 task.`
 };
 // Calling the Function
 console.log(test());
