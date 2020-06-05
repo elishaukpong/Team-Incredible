753a64224978cf10e6deb36a3db720c875c9eb57
Taiwodave <43049722+Taiwodave@users.noreply.github.com>
2020-06-05 16:12:02 +0100

    Updated Taiwodave script

--- scripts/Taiwodave_HNG_03847.js
+++ scripts/Taiwodave_HNG_03847.js
@@ -1,3 +1,3 @@
 
-let userdave = "Hello World, this is Akindele Taiwo with HNGi7 ID HNG-03847 using Javascript for stage 2 task and taiwodavecode@gmail.com.";
+let userdave = "Hello World, this is Akindele Taiwo with HNGi7 ID HNG-03847 using JavaScript for stage 2 task and taiwodavecode@gmail.com.";
     	console.log (userdave);
