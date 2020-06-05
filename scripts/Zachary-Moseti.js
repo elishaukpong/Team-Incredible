d4cda9a1ef3a7710a3cfb7c6230c339630dd436a
ZachyDev <mosetizachary001@gmail.com>
2020-06-05 19:53:08 +0300

    corrected the error

--- scripts/Zachary-Moseti.js
+++ scripts/Zachary-Moseti.js
@@ -1,10 +1,1 @@
-// Declare the intern method
-const internDetails = () => {
-    // declare the intern variables
-    const name = 'Zachary Moseti';
-    const id = 'HNG-05091';
-    const language  = 'JavaScript';
-    return `Hello World, this is ${ name } with HNGi7 ID ${ id } using ${ language } for stage 2 task.`
-}
-// call the function and output the details
-console.log(internDetails());
+console.log("Hello World, this is Zachary Moseti with HNGi7 ID HNG-05091 and email mosetizachary001@gmail.com using JavaScript for stage 2 task.");
