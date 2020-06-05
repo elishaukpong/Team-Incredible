ebab2e0a0debc0fd16678edf1129026d01f690f6
Abdul Barri <lawalabdulbarri@gmail.com>
2020-06-05 20:30:17 +0100

    Update Abdul-Barri.php

--- scripts/Abdul-Barri.php
+++ scripts/Abdul-Barri.php
@@ -9,7 +9,7 @@
 
 	if ($loggedIn == true) {
 
-	echo "Hello World, this is Abdul Barri Lawal with HNGi7 ID HNG-03782 and email lawalabdulbarri@gmail.com using 			PHP for stage 2 task";	
+	echo "Hello World, this is Abdul Barri Lawal with HNGi7 ID HNG-03782 and email lawalabdulbarri@gmail.com using PHP for stage 2 task";	
 
 	}
 
