fa920c4f66cd1cf44a21b3b5a74fb209f9ca80f4
Oluwaseun Michael Oluwatomisin <62293658+ooluwaseunmicheal@users.noreply.github.com>
2020-06-05 16:41:29 +0100

    Script commit

---
+++ scripts/HNG-04643.js
@@ -0,0 +1,9 @@
+
+var mikeTask = {
+                Name: "Oluwaseun Oluwatomisin",
+                ID: "HNG-04643",
+                Email: "ooluwaseunmicheal@gmail.com",
+                Language: "JavaScript"
+              }
+
+console.log("Hello World, this is " +mikeTask.Name+ " with HNGi7 ID " +mikeTask.ID + " and email " +mikeTask.Email+ " using " +mikeTask.Language + " for stage 2 task." );
