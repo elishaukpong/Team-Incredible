132988680fa22b1205c5dbaaaba2dbfcaa8919b7
Wiamer <erem.amarachi@gmail.com>
2020-06-05 15:21:51 +0100

    Updated my name

--- scripts/random_AmarachiErem.py
+++
@@ -1,8 +0,0 @@
-name = "Amarachi Erem"
-email = "erem.amarachi@gmail.com"
-hng_id = "HNG-04424"
-lang = "Python"
-
-output = f"Hello World, this is {name} with HNGi7 ID {hng_id} and email {email} using {lang} for stage 2 task"
-
-print(output)
