11bc02c5d9f883c9022c9af5cc5ee37510df5f94
VictoriaAde <adedayovicky123@gmail.com>
2020-06-05 17:21:34 +0100

    Updated file name

---
+++ scripts/Victoria-Adedayo.js
@@ -0,0 +1,5 @@
+var data = {"first_name":"Victoria", "last_name":"Adedayo", "id":"HNG-01723", "email":"adedayovicky123@gmail.com", "language":"Javascript"};
+
+var j = (data);
+console.log("Hello World, this is " + j.first_name + " " + j.last_name + " with HNGi7 ID " + j.id + " and email " + j.email + " using " + j.language + " for stage 2 task.");
+
