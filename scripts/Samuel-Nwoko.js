f6db979e5804c6427fd18021e5ebd46bab899bed
sahmie <sahmienwoko@gmail.com>
2020-06-05 15:41:06 +0100

    fix typo

--- scripts/Samuel-Nwoko.js
+++ scripts/Samuel-Nwoko.js
@@ -3,8 +3,8 @@ const id = "HNG-01730";
 const emailAddr = "sahmienwoko@gmail.com";
 const language = "JavaScript";
 
-personalIfo = ()=>{
+const personalInfo = ()=>{
   console.log(`Hello World, this is ${fullName} with HNGi7 ID ${id} and email ${emailAddr} using ${language} for stage 2 task.`)
 };
 
-personalIfo();
+personalInfo();
