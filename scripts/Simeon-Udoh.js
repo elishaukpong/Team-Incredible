afc9f4b1f014e6c77055359bf79db49f7a9fc0cc
simeon4real <simeon.udoh45@gmail.com>
2020-06-05 17:28:55 +0100

    :art: updated my script to meet the requirement

--- scripts/Simeon-Udoh.js
+++ scripts/Simeon-Udoh.js
@@ -3,5 +3,5 @@ const name = "Simeon Udoh",
 	language = "Javascript",
 	email = "simeon.udoh45@gmail.com";
 console.log(
-	`Hello World, This is ${name} with HNGi7 ID HNG-${ID} and email ${email} using ${language} for stage 2 task.`
+	`Hello World, this is ${name} with HNGi7 ID HNG-${ID} and email ${email} using ${language} for stage 2 task.`
 );
