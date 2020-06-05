61ea6df4060f793eafdb37e87a8393c234c1ce55
Gozz-Sand <ajachigozie31@gmail.com>
2020-06-05 18:03:28 +0100

    Create Chigozie Aja.py

---
+++ scripts/Chigozie Aja.py
@@ -0,0 +1,10 @@
+# script for HNGi7...
+
+fullname = 'Aja Chigozie'
+ID = 'HNG-02493'
+email = 'ajachigozie31@gmail.com'
+lang = 'Python'
+
+
+print('Hello World, this is', fullname, 'with HNGi7 ID', ID, 'and email', email, 'using', lang, "for stage 2 task.")
+
