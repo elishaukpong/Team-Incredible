d472e66756cc3a99743d89f6378960bce8987290
Ledisi Zabbey <ledisizab@gmail.com>
2020-06-05 19:32:21 +0100

    update

--- scripts/Ledisi-Zabbey.js
+++ scripts/Ledisi-Zabbey.js
@@ -1,5 +1,5 @@
-function newTak() {
-    console.log("Hello World, this is Ledisi Zabbey with HNGi7 ID HNG-01980 using Javascript for stage 2 task and ledisizab@gmail.com");
+function newTask() {
+    console.log("Hello World, this is Ledisi Zabbey with HNGi7 ID HNG-01980 and ledisizab@gmail.com using JavaScript for stage 2 task.");
   }
 
   newTask();
