1bd6f5d059ee22c50187b4002038ff20aeb37377
David-N-Cee <66029306+David-N-Cee@users.noreply.github.com>
2020-06-05 18:23:16 +0100

    David-Chukwuyem.php

---
+++ scripts/David-Chukwuyem.php
@@ -0,0 +1,5 @@
+<?php $my_email = "davidchukwuyem@gmail.com";
+$my_name = "David Chukwuyem";
+$my_hng_id = "HNG-03163";
+$prog_lang = "PHP";
+echo "Hello World, this is " . $my_name . " with HNGi7 ID " . $my_hng_id . " and email " .$my_email. " using " . $prog_lang . " for stage 2 task.";?>
