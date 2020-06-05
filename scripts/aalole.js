c20743229c280a115654fc9f330d3a135837e555
MR-aalole <52821144+Temitope25@users.noreply.github.com>
2020-06-05 18:01:47 +0200

    This is my script, slack ID: 02286
    My submission for the current assignment

---
+++ scripts/aalole.js
@@ -0,0 +1,3 @@
+const aaloleHngDetails = (name, id, email, lang) =>
+  `Hello World, this is ${name} with HNGi7 ID ${id} and email ${email} using ${lang} for stage 2 task`;
+aaloleHngDetails("Rasheed Mikail Abiodun", "HNG-02286", "Rasheedabiodun556@yahoo.com", "Javascript");
