fc0d9b387e97f9f28a736ae22accd030701f14d9
Wiamer <erem.amarachi@gmail.com>
2020-06-05 19:19:05 +0100

    Update Amarachi-Erem.py

--- scripts/Amarachi-Erem.py
+++ scripts/Amarachi-Erem.py
@@ -3,4 +3,4 @@ email = "erem.amarachi@gmail.com"
 hng_id = "HNG-04424"
 lang = "Python"
 
-print(f"Hello World, this is {name} with HNGi7 ID {hng_id} and email {email} using {lang} for stage 2 task.")
+print("Hello World, this is " + name + " with HNGi7 ID " + hng_id + " and email " + email + " using " + lang + " for stage 2 task.")
