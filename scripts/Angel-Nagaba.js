e5bbf84514ac16d5efd126c916b1ca4059e61e62
angelnagaba <restyangel143@gmail.com>
2020-06-05 17:51:06 +0300

    Team incredible task

---
+++ scripts/Angel-Nagaba.js
@@ -0,0 +1,5 @@
+var Name = "Angel Nagaba";
+var Hngi7_ID = "HNG-03520";
+var Lang = "Javascript";
+
+console.log("Hello World, this is "+Name+" "+"with HNGi7 ID "+Hngi7_ID+" "+"and email angelnagaba99@gmail.com using "+Lang+" "+"for stage 2 task.")
