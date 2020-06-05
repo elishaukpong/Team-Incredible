ad07edf678fb054d27b3ac321575d96f71022754
CrazyChickenDev <dannychukz15@gmail.com>
2020-06-05 15:32:06 +0100

    final

---
+++ scripts/Nwaobi-Daniel.js
@@ -0,0 +1,10 @@
+//HNGi7-Stage 2.1 Task
+//Team Incredible
+let email = "dannychukz15@gmail.com";
+let name = "Nwaobi Daniel";
+let myId = "HNG-04069";
+let language = "Javascript";
+
+let output = `Hello World, this is ${name} with HNGi7 ID ${myId} and email ${email} using ${language} for stage 2 task.`;
+
+console.log(output);
