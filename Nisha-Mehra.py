feabc85f95316a10286e9bb2f6e213322fc7fbe9
Nisha Mehra <64705808+nishamehra@users.noreply.github.com>
2020-06-05 07:30:54 -0700

    Delete Nisha-Mehra.py

--- Nisha-Mehra.py
+++
@@ -1,4 +0,0 @@
-name = "Nisha Mehra"
-hngid = "HNG-01532"
-email_address = "nishamehra2052@gmail.com"
-print("Hello World, this is " + name + " with HNGi7 ID " + hngid +" and email " + email_address + " using Python for stage 2 task. ")
