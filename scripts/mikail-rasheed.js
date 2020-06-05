503eaf668e106883d9f43fe224becf99146d672b
MR-aalole <52821144+Temitope25@users.noreply.github.com>
2020-06-05 20:52:11 +0200

    updated aalole script
    full stop added. console.log before and returning the output.

--- scripts/mikail-rasheed.js
+++ scripts/mikail-rasheed.js
@@ -3,8 +3,10 @@ const aaloleHngDetails = () => {
   const id = "HNG-02286";
   const email = "Rasheedabiodun556@yahoo.com";
   const lang = "JavaScript";
-  return `Hello World, this is ${name} with HNGi7 ID ${id} and email ${email} using ${lang} for stage 2 task`;
+  console.log(
+    `Hello World, this is ${name} with HNGi7 ID ${id} and email ${email} using ${lang} for stage 2 task.`
+  );
+  return `Hello World, this is ${name} with HNGi7 ID ${id} and email ${email} using ${lang} for stage 2 task.`;
 };
 
-console.log(aaloleHngDetails());
 aaloleHngDetails();
