af0c255497aa421e8f0283460a68512300609502
Ritika-Agrawal811 <65513097+Ritika-Agrawal811@users.noreply.github.com>
2020-06-05 20:05:28 +0530

    Ritika-Agrawal.php

---
+++ scripts/Ritika-Agrawal.php
@@ -0,0 +1,10 @@
+
+   
+   <?php
+    
+   $id = "HNG-00436";
+    
+    echo "Hello World, this is Ritika Agrawal with HNGi7 ID $id and email ritikaagrawal339@gmail.com using php for stage 2 task.";
+    
+    ?>
+    
