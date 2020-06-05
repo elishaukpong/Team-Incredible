074761fa39b41e082594c950befd5d6dce4136f0
Olutoke Damola <olutokeezekiel18@gmail.com>
2020-06-05 18:25:07 +0100

    create adedamola-olutoke.py

---
+++ scripts/adedamolaOlutoke-HNG-04110.py
@@ -0,0 +1,5 @@
+name = "Olutoke Damola"
+HNG_ID = "HNG-04110"
+language = "Javascript"
+email_address = "olutokeezekiel18@gmail.com"
+print("Hello world, this is {} with HNGi7 ID {} and email {} using {} for stage 2 task".format(name, HNG_ID, email_address, language))
