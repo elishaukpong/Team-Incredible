c41f723d589afe4ce9164f0e046666d1d944c53e
Tobi Osimosu <Osimosutobi@gmail.com>
2020-06-05 15:54:14 +0100

    Update Joshua-Paul.php

--- scripts/Joshua-Paul.php
+++ scripts/Joshua-Paul.php
@@ -5,4 +5,4 @@ $myid = "HNG-05491";
 $mylanguage = "PHP";
 $myemail = "veecthorpaul@gmail.com";
 
-print(" Hello world, this is {$myname} with HNGi7 ID {$myid} and email {$myemail} using {$mylanguage} for stage 2 task ");
+print(" Hello World, this is {$myname} with HNGi7 ID {$myid} and email {$myemail} using {$mylanguage} for stage 2 task ");
