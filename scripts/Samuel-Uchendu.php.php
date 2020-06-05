82366ad2a456ab6d2fc84f430683ffb5ed50690c
samueluchendu <samueluchendu47@gmail.com>
2020-06-05 18:32:52 +0100

    added my script

---
+++ scripts/Samuel-Uchendu.php.php
@@ -0,0 +1,23 @@
+<?php
+
+
+function viewDetails(){
+
+     $id = 'HNG-01162';
+  $email = 'samueluchendu47@gmail.com';
+$language = 'PHP';
+$fullname = 'Samuel Uchendu';
+
+
+  echo 'Hello World, this is '.$fullname. ' with HNGi7 ID '.$id.' and email '.$email.' using '.$language.' for stage 2 task';
+
+
+}
+
+viewDetails();
+
+
+
+
+
+ ?>
