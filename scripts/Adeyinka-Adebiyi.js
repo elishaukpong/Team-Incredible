1b1a1dbe2b5452c820b2007d10181fefcbd9bee5
Yinkash100 <yinkash100@gmail.com>
2020-06-05 16:25:22 +0100

    Added my script changed it to match convention

--- scripts/Adeyinka-Adebiyi.js
+++ scripts/Adeyinka-Adebiyi.js
@@ -2,5 +2,5 @@ const name = "Adeyinka Adebiyi";
 const hng_id = "HNG-04012";
 const email = "yinkash1000@gmail.com";
 console.log(
-  `Hello World, this is ${name} with HNGi7 ID ${hng_id} using JavaScript for stage 2 task ${email}`
+  `Hello World, this is ${name} with HNGi7 ID ${hng_id} and email ${email} using JavaScript for stage 2 task`
 );
