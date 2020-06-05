3df8a2841c44d8a3aa382560e644398b6dbdb3ac
LaminMinteh <54646056+LaminMinteh@users.noreply.github.com>
2020-06-05 17:48:43 +0000

    Update Lamin-Minteh.py

--- scripts/Lamin-Minteh.py
+++ scripts/Lamin-Minteh.py
@@ -4,6 +4,6 @@ lang = 'Python'
 email = 'mintehl6@gmail.com'
 
 print(
-    "Hello World, this is {} with HNGi7 ID {} and email address {} using {} for stage 2 task." .format(fName,hngid,email,lang))
+    "Hello World, this is {} with HNGi7 ID {} and email {} using {} for stage 2 task." .format(fName,hngid,email,lang))
 
     
