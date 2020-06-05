6396949f2a640638b6e8f585ca471f61e9d701eb
raiyan-aegon <mukhtarraiyan16994@gmail.com>
2020-06-05 18:48:03 +0000

    Changes to Script

--- scripts/Raiyan-Mukhtar.py
+++ scripts/Raiyan-Mukhtar.py
@@ -1,9 +1,3 @@
-name = "Raiyan Mukhtar"
-id = "HNG-01816"
-language ="python"
-
-print("Hello world, this is {0} with HNGi7 ID {1} using {2} for stage 2 task.".format(name,id,language))
-
 
 name = "Raiyan Mukhtar"
 id = "HNG-01816"
