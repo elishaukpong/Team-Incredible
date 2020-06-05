0c165fd130f6c38c9ec5b55accc860d22704baff
Jeth0214 <rolandjethrosuyom@yahoo.com>
2020-06-05 17:42:11 +0300

    created RolanJethro-Suyom.js file

--- scripts/RolandJethro-Suyom.js
+++ scripts/RolandJethro-Suyom.js
@@ -1,4 +1,4 @@
-// Output Format
+// Output Sample
 //"Hello World, this is Elisha Ukpong with HNGi7 ID HNG-03659 and email ishukpong418@gmail.com using JavaScript for stage 2 task."
 
 const fullName = 'Roland Jethro Suyom',
@@ -6,6 +6,6 @@ const fullName = 'Roland Jethro Suyom',
       email = 'rolandjethrosuyom@yahoo.com',
       language = 'JavaScript';
 
-const details = `Hello World, this is ${fullName} with HNGi7 ID ${id} and email ${email} using ${language} for stage 2 task`;
+const details = `Hello World, this is ${fullName} with HNGi7 ID ${id} and email ${email} using ${language} for stage 2 task.`;
 
 console.log(details)
