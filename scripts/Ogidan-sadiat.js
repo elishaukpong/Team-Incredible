7ad6550430c33df5d167b2d2fbf595ca3f4d8c48
halimatsadiat <haleymaogidan@gmail.com>
2020-06-05 15:58:12 +0100

    add ogidansadiat script

---
+++ scripts/Ogidan-sadiat.js
@@ -0,0 +1,9 @@
+var info = [];
+info["myFullName"] = "Ogidan Sadiat";
+info["ID"] = "HNG-00834";
+info["language"] = "Javascript";
+var myMail = [];
+myMail["email"] = "haleymaogidan@gmail.com";
+
+
+console.log("Hello World, this is " + info["myFullName"] + " with HNGi7 ID " + info["ID"] + " and email " + myMail["email"] + " using " + info["language"] + " for stage 2 task.");
