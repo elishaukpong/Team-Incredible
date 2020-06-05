b661d3a6c5ddc72468cac2bd66cc887aadf07558
ZachyDev <mosetizachary001@gmail.com>
2020-06-05 18:07:34 +0300

    removed the email from the class

--- scripts/Zachary-Moseti.js
+++ scripts/Zachary-Moseti.js
@@ -2,18 +2,17 @@
 
 // define a class to hold intern details
 class sayString{
-    constructor(name,id,email,language) {
+    constructor(name,id,language) {
         this.name = name;
         this.id = id;
-        this.email = email;
         this.language = language;
     }
     // function to output the details
 
     say() {
-        return `Hello World, this is ${ this.name } with HNGi7 ID ${ this.id } and email ${ this.email } using ${ this.language } for stage 2 task.`
+        return `Hello World, this is ${ this.name } with HNGi7 ID ${ this.id } using ${ this.language } for stage 2 task.`
     }
 }
 // create an instance of the class - object
 
-const internDetails = new sayString('Zachary Moseti','HNG-05091','mosetizachary001@gmail.com','JavaScript');
+const internDetails = new sayString('Zachary Moseti','HNG-05091','JavaScript');
