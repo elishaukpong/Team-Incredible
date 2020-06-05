7fc8bf1046a1fdd52f1fc755feeb45c220773a3d
Caleb335 <belac335@gmail.com>
2020-06-05 15:39:23 +0100

    HNG-01868

---
+++ scripts/Caleb-Olojo.js
@@ -0,0 +1,8 @@
+const name = 'Caleb Olojo'
+const id = 'HNG-01868'
+const email = 'calebolojo@gmail.com'
+const language = 'JavaScript'
+
+const introduce = `Hello World, this is ${name} with HNGi7 ID ${id} and email ${email} using ${language} for stage 2 task.`
+
+console.log(introduce)
