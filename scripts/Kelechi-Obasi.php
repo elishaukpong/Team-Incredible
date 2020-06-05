ca0cc3387fbfe966b679b4d87ec5ae8c461e864c
kay-ai <favourobasi6@gmail.com>
2020-06-05 15:58:44 +0100

    Merge my script

---
+++ scripts/Kelechi-Obasi.php
@@ -0,0 +1,8 @@
+<?php 
+    $fullname = "Kelechi Obasi";
+    $id = "HNG-04349";
+    $language = "Laravel";
+    $email = "favourobasi6@gmail.com";
+
+    echo 'Hello World, this is' . ' ' . $fullname . ' ' . 'with HNGi7 ID' . ' ' . $id . ' ' . 'and' . ' ' . 'email' . ' ' . $email . ' ' . 'using' . ' ' .$language . ' ' . 'for stage 2 task.';
+?>
