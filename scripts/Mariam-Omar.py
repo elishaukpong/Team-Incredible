660a40131cb33203e2eb69b5b22ce75933673800
samiyah1 <umarsamiya@gmail.com>
2020-06-05 20:16:49 +0300

    Mariam Omar HNG-01084 script

---
+++ scripts/Mariam-Omar.py
@@ -0,0 +1,9 @@
+information = "Hello World! This is Mariam Omar with "
+ID = " ID HNG-01084"
+email = "umarsamiya@gmail.com"
+language = "python language"
+stage = "stage 2"
+status = "pass"
+
+
+print(information + " " +ID +  " and  my email is " +  email  +" using " + language + " for "+ stage  )
