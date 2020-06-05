36aabef10059adbf963880706a3efbc5f736dc91
Qozeem Odeniran <muhdqozeem@gmail.com>
2020-06-05 15:57:54 +0100

    Updated

--- scripts/qozeem1.php
+++ scripts/qozeem1.php
@@ -2,6 +2,6 @@
 $fullname = "Qozeem Odeniran";
 $id = "HNG-01681";
 $language = "PHP";
-
-echo "Hello World, this is $fullname with HNGi7 ID $id using $language for stage 2 task.muhdqozeem@gmail.com";
+$email = "muhdqozeem@gmail.com";
+echo "Hello World, this is $fullname with HNGi7 ID $id  and email $email using $language for stage 2 task.";
 ?>
