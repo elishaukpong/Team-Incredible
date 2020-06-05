2079ff95a0188fa6bb5a8f3f28a3f52b00ea7523
Yinkash100 <yinkash100@gmail.com>
2020-06-05 15:30:19 +0100

    Added my script -yinkash

---
+++ scripts/Yinkash.js
@@ -0,0 +1,6 @@
+const name = "Adeyinka Adebiyi";
+const hng_id = "HNG-04012";
+const email = "yinkash1000@gmail.com";
+console.log(
+  `Hello World, this is ${name} with HNGi7 ID ${hng_id} using JavaScript for stage 2 task ${email}`
+);
