70b3707383ffdfeb2263c8e38f609d85f27dab2c
Vivyanne-504 <66225228+Vivyanne-504@users.noreply.github.com>
2020-06-05 19:30:24 +0100

    Update vivian-nwosu.py

--- scripts/vivian-nwosu.py
+++ scripts/vivian-nwosu.py
@@ -1,8 +1,5 @@
 name = "Vivian Nwosu"
-email = "viviannwosu05@gmail.com"
-hng_id = "HNG-00494"
-lang = "Python"
-
-output = f"Hello World, this is {name} with HNGi7 ID {hng_id} and email {email} using {lang} for stage 2 task."
-
-print(output)
+    ID = "HNG-00494"
+    email = "vivianwosu05@gmail.com "
+    language="Python"
+    print('Hello World, this is' +name+'with HNGi7 ID'+ID+ 'and email' +email+ 'using'+language+ 'for stage 2 task')
