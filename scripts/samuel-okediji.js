2f6b1c400f2f2bfab8eefd41cd796ace595989f5
Tobi Osimosu <Osimosutobi@gmail.com>
2020-06-06 01:44:46 +0100

    Update samuel-okediji.js

--- scripts/samuel-okediji.js
+++ scripts/samuel-okediji.js
@@ -3,4 +3,4 @@ const email = 'samuelcenaokediji@gmail.com';
 const hngId = 'HNG-06326';
 const lang  = 'JavaScript';
 
-console.log(`Hello World, this is ${fname} with HNGi7 ID ${hngId} and email ${email} using ${lang} nodeJs for stage 2 task`);
+console.log(`Hello World, this is ${fname} with HNGi7 ID ${hngId} and email ${email} using ${lang} for stage 2 task`);
