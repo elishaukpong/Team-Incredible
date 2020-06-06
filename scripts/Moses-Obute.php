c1b9cedbc397220375e753b492fe44a2ae284b51
obute-agbo <obutemoses5@gmail.com>
2020-06-06 09:31:43 +0100

    Corrected email address Slack ID: mosiac05

--- scripts/Moses-Obute.php
+++ scripts/Moses-Obute.php
@@ -1,10 +1,10 @@
 <?php
 
-$email='obutemoses05@gmail.com';
+$email='obutemoses5@gmail.com';
 $name='Obute Moses';
 $id='HNG-05246';
 $language='PHP';
 
-echo "Hello World, this is Obute Moses with HNGi7 ID HNG-05246 and email obutemoses05@gmail.com using PHP for stage 2 task.";
+echo "Hello World, this is Obute Moses with HNGi7 ID HNG-05246 and email obutemoses5@gmail.com using PHP for stage 2 task.";
 
 ?>
