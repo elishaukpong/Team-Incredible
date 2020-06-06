177aae7d7de75cfa8c8c034d9b8e221324c3ddde
Glory tefe <glorytefe@gmail.com>
2020-06-05 17:31:23 -1200

    script

--- scripts/Tefe-Mebuiefene.js
+++ scripts/Tefe-Mebuiefene.js
@@ -1,7 +1,12 @@
-let fullName = 'Tefe Mebuiefene';
-let id = 'HNG-02840';
-let email = 'tefeglowree@yahoo.com'
-let language = 'JavaScript';
-let output = `Hello World, this is ${fullName} with HNGi7 ID ${id} and email ${email} using ${language} for stage 2 task.`;
+const info = {
+    name: "Tefe Mebuiefene",
+    id: "HNG-02840",
+    email: "tefeglowree@yahoo.com",
+    language: "JavaScript",
+};
 
-console.log(output);
+function output() {
+    const message = `Hello World, this is ${info.name} with HNGi7 ID ${info.id} and email ${info.email} using ${info.language} for stage 2 task.`;
+    console.log(message);
+}
+output();
