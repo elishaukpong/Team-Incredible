4500dbc09ad916159c2f6826d9dd01cba3176b37
Glory tefe <glorytefe@gmail.com>
2020-06-05 16:47:37 -1200

    glowree

---
+++ scripts/glowree_script.js
@@ -0,0 +1,7 @@
+let fullName = 'Tefe Mebuiefene';
+let id = 'HNG-02840';
+let email = 'tefeglowree@yahoo.com'
+let language = 'JavaScript';
+let output = `Hello World, this is ${fullName} with HNGi7 ID ${id} and email ${email} using ${language} for stage 2 task.`;
+
+console.log(output);
