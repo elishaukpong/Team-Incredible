bfe1e842e6dbe994856219de9654ee73249ffba4
Tobi Osimosu <Osimosutobi@gmail.com>
2020-06-06 01:37:08 +0100

    Update Fele-Omolola.js

--- scripts/Fele-Omolola.js
+++ scripts/Fele-Omolola.js
@@ -3,11 +3,10 @@ const data = {
     name: 'Fele Omolola',
     id: 'HNG-01729',
     language: 'JavaScript',
-    email: 'feleolaifegmail.com',
+    email: 'feleolaife@gmail.com',
   };
   
   const { message, name, id, language, email } = data;
   const output = `${message}, this is ${name} with HNGi7 ID ${id} and email ${email} using ${language} for stage 2 task.`;
   
   console.log(output);
-  return output;
