31e94542436839f393ce690d1f60876209ef205a
kanzytech <53080328+kanzytech@users.noreply.github.com>
2020-06-06 12:38:02 +0100

    Create Nsikan_work.js

---
+++ Nsikan_work.js
@@ -0,0 +1,13 @@
+// storing variables in an object
+const intern = {
+  firstName: 'Nsikan',
+  lastName: 'Effiong',
+  hngId: 'HNG-05267',
+  language: 'JavaScript',
+  email: 'nsibest17@gmail.com'
+}
+
+// output show message
+const showMessage = `Hello World, this is ${intern.lastName} ${intern.firstName} with HNGi7 ID ${intern.hngId} and email ${intern.email} using ${intern.language} for stage 2 task.`;
+
+console.log(showMessage);                                                                                 
