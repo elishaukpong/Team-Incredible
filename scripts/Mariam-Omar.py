12f0956c94a6a8c230b79586c57f2bba0e2549c5
Elisha Ukpong <Ishukpong418@gmail.com>
2020-06-06 09:05:55 +0100

    fixed

--- scripts/Mariam-Omar.py
+++ scripts/Mariam-Omar.py
@@ -1,7 +1,7 @@
-message = "Hello World! This is Mariam Omar with ID HNG-01084"
+message = "Hello World, this is Mariam Omar with HNGi7 ID HNG-01084"
 email = "email umarsamiya@gmail.com"
 language = "python"
-stage = "stage 2"
+stage = "stage 2 task."
 
 
-print(message + " " + " and  " +  email  +" using " + language + " for "+ stage  )
+print(message + " and " +  email + " using " + language + " for "+ stage )
